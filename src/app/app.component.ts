e80c4536f3bfededeb4fc710cf04237bcd45cc25
geetha19983b <geethainfy@gmail.com>
2021-05-03 22:39:41 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { MockForm } from "./shared/mock/mock-form";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  data = MockForm;
+}
